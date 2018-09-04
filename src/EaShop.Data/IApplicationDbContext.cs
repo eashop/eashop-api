ea0ef61220ac9839fb616bfc9e476df68a1758ac
DeadSith <deadsith@hotmail.com>
2018-09-04 22:06:55 +0300

    Added basic skeleton for project

---
+++ src/EaShop.Data/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using EaShop.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace EaShop.Data
+{
+    public interface IApplicationDbContext
+    {
+        
+    }
+}
