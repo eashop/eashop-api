3c044471f2a1a8b328c3b1525b89e4b3e80ab557
Vladyslav Ardelyan <ardelyan.vladyslav@gmail.com>
2018-11-13 10:34:04 +0200

    Added method for search product by name.

--- src/EaShop.Api/Controllers/GoodsController.cs
+++ src/EaShop.Api/Controllers/GoodsController.cs
@@ -75,6 +75,52 @@ namespace EaShop.Api.Controllers
             return Ok(goods);
         }
 
+        // GET: api/Goods/search
+        [HttpPost("search")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Goods>))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult SearchGoods([FromBody] SearchWithPagination search)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (string.IsNullOrWhiteSpace(search.Name))
+            {
+                return BadRequest("Name is invalid");
+            }
+
+            var goods = _context.Goods
+                .Where(g => g.Name.ToLowerInvariant().Contains(search.Name.ToLowerInvariant()));
+
+            if (goods == null)
+            {
+                return NotFound();
+            }
+
+            if (search.PageSize == null || search.PageNumber == null)
+            {
+                return Ok(goods);
+            }
+            else
+            {
+                try
+                {
+                    var result = goods
+                        .Skip((int)search.PageSize * ((int)search.PageNumber - 1))
+                        .Take((int)search.PageSize);
+                    return Ok(result);
+                }
+                catch
+                {
+                    return BadRequest(search);
+                }
+            }
+
+        }
+
         // PUT: api/Goods/5
         [HttpPut("{id}")]
         [Authorize(Roles = "Admin")]
